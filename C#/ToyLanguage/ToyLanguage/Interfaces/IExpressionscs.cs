a9fb6c0aa13a53158de0209b24f0aec3847cd16e
clapa lucian <clapa_lucian@outlook.com>
2015-10-26 19:19:48 +0200

    initial commit for c# project

---
+++ C#/ToyLanguage/ToyLanguage/Interfaces/IExpressionscs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyLanguage.Interfaces
+{
+    interface IExpressionscs
+    {
+        int eval(IMyDictionary<string, int> table);
+
+        string toString();
+    }
+}
---
+++ C#/ToyLanguage/ToyLanguage/Interfaces/IList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyLanguage.Interfaces
+{
+    interface IList<T>
+    {
+        /**
+     * Add a new element in list
+     *
+     * @param element T element
+     */
+        void add(T element);
+
+        /**
+         * Get the element from list by position
+         *
+         * @param position Int position
+         * @return T element
+         */
+        T get(int position);
+
+        /**
+         * Remove element T at position
+         *
+         * @param position Int position
+         */
+        void remove(int position);
+
+        /**
+         * Check if list is empty
+         *
+         * @return True if empty, false otherwise
+         */
+        bool isEmpty();
+
+        /**
+         * Get the size of the list
+         *
+         * @return Int size
+         */
+        int size();
+    }
+}
