59532a34beb05846a386875d256977582e214146
Clapa Lucian <clapa_lucian@yahoo.com>
2016-01-11 21:24:53 +0200

    changed some names

--- Java/src/model/Collections/MyHeap.java
+++ Java/src/model/Collections/MyHeap.java
@@ -2,10 +2,13 @@ package model.Collections;
 
 import interfaces.IHeap;
 
+import java.util.Map;
+
 /**
  * Created by Lucian on 11/21/2015.
  */
 public class MyHeap extends WrapperDictionary<Integer, Integer> implements IHeap<Integer, Integer> {
     //The heap hass same implementation as the dictionary because it is
     //using the key->value structure just with different data types
+
 }
